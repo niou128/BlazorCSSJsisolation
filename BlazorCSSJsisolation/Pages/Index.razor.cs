d8659f0388b5250c0df8a89d96a2fd846585e3ca
Sébastien <niou128@gmail.com>
2021-02-18 18:34:58 +0100

    js isolation

--- BlazorCSSJsisolation/Pages/Index.razor.cs
+++ BlazorCSSJsisolation/Pages/Index.razor.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.JSInterop;
 using System.Threading.Tasks;
 
 namespace BlazorCSSJsisolation.Pages
 {
     public class IndexBase : ComponentBase
     {
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
+
+        private Task<IJSObjectReference> _module;
+
+        private Task<IJSObjectReference> Module => _module ??= JSRuntime.InvokeAsync<IJSObjectReference>("import", "./tools.js").AsTask();
+
+        protected async Task SayHello()
+        {
+            var module = await Module;
+            await module.InvokeVoidAsync("HelloWorld");
+        }
     }
 }
